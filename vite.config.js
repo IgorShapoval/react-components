0be69d4a3225084c878d17e7ce9313ad91ed3db0
IgorShapoval <igorshapoval92@gmail.com>
2023-05-18 17:09:56 +0300

    edit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://igorshapoval.github.io/react-components/dist",
 });
